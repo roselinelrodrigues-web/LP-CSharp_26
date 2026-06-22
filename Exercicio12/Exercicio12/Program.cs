ad654ce3b206bc9e315da400641e19e7f424e084
roselinelrodrigues-web <roselinelrodrigues@gmail.com>
2026-06-22 11:32:47 +0100

    Exercicio12

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    public class Program
+    {
+    public static void Main(string[] args)
+        {
+            int somaFinal = 0;
+            int i;
+            for (i = 1; somaFinal <= 100; i++)
+            {
+                somaFinal += i;
+                Console.WriteLine(somaFinal);
+            }
+            Console.WriteLine("A somaFinal é " + i);
+        }
+    }
+
+
