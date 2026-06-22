06a1af86161638811e87fa0ffe2eefc6908cd15e
roselinelrodrigues-web <roselinelrodrigues@gmail.com>
2026-06-22 13:32:09 +0100

    Exercicio13

---
+++ Exercicio13/Exercicio13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    public class Exercicio13
+    {
+        public static void Main(string[] args)
+        {
+        int i = 1;
+        do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 5);
+        Console.WriteLine("contagem concluida!");
+        }
+    }
+
